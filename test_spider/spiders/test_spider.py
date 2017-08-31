f89893dde5b8bc23ead6e457e4cfcf227a77c765
shangguzhen <347306165@qq.com>
2017-08-31 21:39:45 +0800

    complete HouseItem

--- test_spider/spiders/test_spider.py
+++ test_spider/spiders/test_spider.py
@@ -18,26 +18,38 @@ class test_spider(CrawlSpider):
     
     def parse_new_house(self,response):
         self.logger.info(reponse.url);
-        house_items=response.xpath("//ul[@id='newhouse-list']//i[@class='clearfix']");
+        house_items=response.xpath("//ul[@id='newhouse-list']//li[@class='clearfix']");
         
         items=[];
         for house_item in house_items:
             item=HouseItem();
-            item["name"]=house_item.xpath("");
-            item["alias"]=house_item.xpath("");
-            item["desc"]=house_item.xpath("");
-            item["price"]=house_item.xpath("");
-            item["discount"]=house_item.xpath("");
-            item["house_type"]=house_item.xpath("");
-            item["open_qt_time"]=house_item.xpath("");
-            item["close_qt_time"]=house_item.xpath("");
-            item["addr"]=house_item.xpath("");
-            item["is_on_sell"]=house_item.xpath("");
-            item["tag"]=house_item.xpath("");
-            item["url"]=house_item.xpath("");
-            item["phone_number"]=house_item.xpath("");
-            item["house_pic"]=house_item.xpath("");
-            item["house_type_pic"]=house_item.xpath("");
+            
+            title_sel=house_item.xpath("div[@class='show-detail fl']/p[1]");
+            item["name"]=title_sel.xpath("a")[0].extract();
+            item["discount"]=title_sel.xpath("span/em")[0].extract();
+            item["is_on_sell"]=title_sel.xpath("i")[0].extract();
+            item["url"]=house_item.xpath("a@href")[0].extract();
+            
+            dtl_sel=house_item.xpath("div[@class='show-detail fl']/div[1]");
+            item["alias"]=dtl_sel.xpath("p[1]/span[2]")[0].extract();
+            item["desc"]="";    #dtl_sel.xpath("")[0].extract();
+            item["room_types"]=dtl_sel.xpath("p[2]/span[2]")[0].extract();
+            item["open_qt_time"]=dtl_sel.xpath("div[1]/p[1]/span[1]")[0].extract();
+            item["close_qt_time"]=dtl_sel.xpath("div[1]/p[1]/span[2]")[0].extract();
+            item["zone"]=dtl_sel.xpath("p[3]/span[2]")[0].extract();
+            item["addr"]=dtl_sel.xpath("p[3]/span[2]/em")[0].extract();
+            item["tag"]=" ".join(tag.extract() for tag in dtl_sel.xpath("div/div[2]/p"));
+            
+            oth_inf_sel=house_item.xpath("div[@class='show-price']");
+            item["price"]=oth_inf_sel.xpath("span[1]")[0].extract();
+            item["price_unit"]=oth_inf_sel.xpath("span[2]")[0].extract();
+            item["phone_number"]=oth_inf_sel.xpath("p[1]/i")[0].extract();
+            item["house_type_pic"]=[img.extract() for img in oth_inf_sel.xpath("div[@class='dm-photo']/ul/li/a/img@src")];
+            
+            photo_sel=house_item.xpath("p[@class='house-photo fl']");
+            item["house_pic"]=photo_sel.xpath("a/img@src")[0].extract();
+            item["house_type"]=photo_sel.xpath("span/em")[0].extract();
+            
             items.append(item);
         return items;
     
